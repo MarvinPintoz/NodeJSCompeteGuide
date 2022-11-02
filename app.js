4cf16f5c5fbc98b9951f4629bf48f8153b3e4ada
MarvinPintoz <marvinr_pintoz@hotmail.com>
2022-11-02 14:31:32 -0600

    Seccion 32. Routing Request Course node js

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ const server = http.createServer((req, res) => {
     //console.log(req);
     console.log(req.url, req.method, req.headers);
     //process.exit(); 
+    const url = req.url;
+
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
